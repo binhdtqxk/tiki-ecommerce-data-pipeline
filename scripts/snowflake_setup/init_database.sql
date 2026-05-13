ba99992c790a62cf995da3250b3c4ed0a48304b0
binhdtqxk <146164453+binhdtqxk@users.noreply.github.com>
2026-05-13 20:12:13 +0700

    Create database and schema in snowflake

---
+++ scripts/snowflake_setup/init_database.sql
@@ -0,0 +1,13 @@
+create database if not exists tiki_db;
+use database tiki_db;
+
+--Create staging (contain raw data from asw s3)
+create schema if not exists staging;
+
+--Create warehouse (contain dim and fact table)
+create schema if not exists core;
+
+--Create staging that point to asw s3 bucket
+use schema staging;
+create or replace stage my_s3_tiki_stage
+url='s3://binh-tiki-data-pipeline-646984527458-ap-southeast-1-an/raw_data/tiki'
