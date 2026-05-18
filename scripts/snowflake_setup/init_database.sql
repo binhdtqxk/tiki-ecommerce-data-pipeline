19e4c4a2b08196eee44dc721b133f94b872b7b3a
binhdtqxk <146164453+binhdtqxk@users.noreply.github.com>
2026-05-18 21:12:21 +0700

    finish storage integration

--- scripts/snowflake_setup/init_database.sql
+++ scripts/snowflake_setup/init_database.sql
@@ -10,4 +10,5 @@ create schema if not exists core;
 --Create staging that point to asw s3 bucket
 use schema staging;
 create or replace stage my_s3_tiki_stage
-url='s3://binh-tiki-data-pipeline-646984527458-ap-southeast-1-an/raw_data/tiki'
+url='s3://binh-tiki-data-pipeline-646984527458-ap-southeast-1-an/raw_data/tiki/'
+STORAGE_INTEGRATION = s3_tiki_integration;
